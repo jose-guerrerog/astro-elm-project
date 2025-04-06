bc62d0eccb3f311e77dd64119afc04e8de56729b
jose-guerrerog <joseguerrerog880@gmail.com>
2025-04-07 05:17:24 +1000

    added tests

---
+++ tests/ProductCardTest.elm
@@ -0,0 +1,128 @@
+module ProductCardTest exposing (suite)
+
+import Expect
+import ProductCard exposing (Model, Msg(..), init, update, view)
+import ProductData exposing (Product, getWallets)
+import Test exposing (Test, describe, test)
+import Test.Html.Query as Query
+import Test.Html.Selector as Selector
+
+
+-- Get the first product for testing
+testProduct : Product
+testProduct =
+    List.head getWallets
+        |> Maybe.withDefault
+            { id = 0
+            , name = ""
+            , category = ""
+            , price = 0
+            , salePrice = Nothing
+            , description = ""
+            , images = []
+            , colorImages = []
+            , colors = []
+            , features = []
+            , isInStock = False
+            , rating = 0
+            }
+
+
+initialModel : Model
+initialModel =
+    init testProduct
+
+
+suite : Test
+suite =
+    describe "ProductCard"
+        [ describe "init"
+            [ test "initializes with the correct product" <|
+                \_ ->
+                    let
+                        model =
+                            init testProduct
+                    in
+                    Expect.equal model.product testProduct
+            , test "initializes with default values" <|
+                \_ ->
+                    let
+                        model =
+                            init testProduct
+                    in
+                    Expect.all
+                        [ \m -> Expect.equal m.isHovered False
+                        , \m -> Expect.equal m.currentImageIndex 0
+                        , \m -> Expect.equal m.isQuickViewOpen False
+                        , \m -> Expect.equal m.selectedColorIndex 0
+                        ]
+                        model
+            ]
+        , describe "update"
+            [ test "MouseEnter sets isHovered to True" <|
+                \_ ->
+                    let
+                        ( updatedModel, _ ) =
+                            update MouseEnter initialModel
+                    in
+                    Expect.equal updatedModel.isHovered True
+            , test "MouseLeave sets isHovered to False" <|
+                \_ ->
+                    let
+                        hoveredModel =
+                            { initialModel | isHovered = True }
+
+                        ( updatedModel, _ ) =
+                            update MouseLeave hoveredModel
+                    in
+                    Expect.equal updatedModel.isHovered False
+            , test "SelectColor updates the selected color index" <|
+                \_ ->
+                    let
+                        ( updatedModel, _ ) =
+                            update (SelectColor 2) initialModel
+                    in
+                    Expect.equal updatedModel.selectedColorIndex 2
+            ]
+        , describe "view"
+            [ test "renders the product name" <|
+                \_ ->
+                    if testProduct.name /= "" then
+                        view initialModel
+                            |> Query.fromHtml
+                            |> Query.find [ Selector.class "product-name" ]
+                            |> Query.has [ Selector.text testProduct.name ]
+                    else
+                        Expect.pass
+            , test "renders color options if available" <|
+                \_ ->
+                    if List.length testProduct.colors > 0 then
+                        view initialModel
+                            |> Query.fromHtml
+                            |> Query.findAll [ Selector.class "color-option" ]
+                            |> Query.count (Expect.equal (List.length testProduct.colors))
+                    else
+                        Expect.pass
+            , test "renders the product description" <|
+                \_ ->
+                    if testProduct.description /= "" then
+                        view initialModel
+                            |> Query.fromHtml
+                            |> Query.find [ Selector.class "product-description" ]
+                            |> Query.has [ Selector.text testProduct.description ]
+                    else
+                        Expect.pass
+            ]
+        , describe "hover behavior"
+            [ test "shows 'SHOW INSIDE +' button when hovered" <|
+                \_ ->
+                    let
+                        hoveredModel =
+                            { initialModel | isHovered = True }
+                    in
+                    view hoveredModel
+                        |> Query.fromHtml
+                        |> Query.find [ Selector.class "show-inside-btn" ]
+                        |> Query.has [ Selector.text "SHOW INSIDE +" ]
+            ]
+        ]
